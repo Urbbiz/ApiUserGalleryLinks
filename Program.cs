721badd81deecfac02f5d27fbf878ed73a4d46c4
Urbbiz <71328179+Urbbiz@users.noreply.github.com>
2021-12-14 18:46:09 +0200

    get user Id

--- Program.cs
+++ Program.cs
@@ -1,8 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-/*
+﻿/*
    1. Sukurti Konsolinę Applikaciją, kuri atspausdina visas nuotraukų nuorodas paimtas iš RestAPI
    https://jsonplaceholder.typicode.com/ kurios priklauso 'Mrs. Dennis Schulist'. Patarimas, naudokite HttpClient klasę.
    Extra: programoje panaudokite Linq Selectus.
    */
 
+using ApiUserGalleryLinks.Models;
+using Newtonsoft.Json;
+using System.Linq;
 
+var httpClient = new HttpClient();
+
+var httpResponse = await httpClient.GetAsync("https://jsonplaceholder.typicode.com/users");
+
+// get user id
+if (httpResponse.IsSuccessStatusCode)
+{
+    var contentString = await httpResponse.Content.ReadAsStringAsync();
+
+    var users = JsonConvert.DeserializeObject<List<User>>(contentString);
+    
+    var userID = users.Where(u => u.Name == "Mrs. Dennis Schulist").FirstOrDefault();
+}
