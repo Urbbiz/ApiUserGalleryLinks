c9633665113714e00c765f297faeb3300798ab1c
Urbbiz <71328179+Urbbiz@users.noreply.github.com>
2021-12-14 16:43:32 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+/*
+   1. Sukurti Konsolinę Applikaciją, kuri atspausdina visas nuotraukų nuorodas paimtas iš RestAPI
+   https://jsonplaceholder.typicode.com/ kurios priklauso 'Mrs. Dennis Schulist'. Patarimas, naudokite HttpClient klasę.
+   Extra: programoje panaudokite Linq Selectus.
+   */
+
+
