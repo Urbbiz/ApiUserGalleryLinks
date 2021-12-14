267160e494f0b2c5293ead152d526f84442152e8
Urbbiz <71328179+Urbbiz@users.noreply.github.com>
2021-12-14 19:52:01 +0200

    added album and photo model,get album id, and get photo url

--- Program.cs
+++ Program.cs
@@ -19,5 +19,36 @@ if (httpResponse.IsSuccessStatusCode)
 
     var users = JsonConvert.DeserializeObject<List<User>>(contentString);
     
-    var userID = users.Where(u => u.Name == "Mrs. Dennis Schulist").FirstOrDefault();
+    var specificUser = users.Where(u => u.Name == "Mrs. Dennis Schulist").FirstOrDefault();
+
+    // get Album Id
+    httpResponse = await httpClient.GetAsync("https://jsonplaceholder.typicode.com/albums");
+
+    if (httpResponse.IsSuccessStatusCode)
+    {
+        contentString = await httpResponse.Content.ReadAsStringAsync();
+
+        var albums = JsonConvert.DeserializeObject<List<Album>>(contentString);
+
+        var specificAlbum = albums.Where(a => a.UserId == specificUser.Id).FirstOrDefault();
+
+        // to get from the photo urls of photos
+        httpResponse = await httpClient.GetAsync("https://jsonplaceholder.typicode.com/photos");
+
+        if (httpResponse.IsSuccessStatusCode)
+        {
+            contentString = await httpResponse.Content.ReadAsStringAsync();
+
+            var photos = JsonConvert.DeserializeObject<List<Photo>>(contentString);
+
+            var specificPhotosUrl = photos.Where(p => p.AlbumId == specificAlbum.UserId);
+
+            foreach(var oneByOneUrl in specificPhotosUrl)
+            {
+                Console.WriteLine($"User name: {specificUser.Name} album id: {specificAlbum.UserId} url:{oneByOneUrl.Url}");
+            }
+        }
+    }
 }
+
+
